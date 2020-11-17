f6d5dcdfe59c7ea584d6630c9a02e8e97592473f
Alex Curran <alexcurran.va@gmail.com>
2020-11-17 10:54:39 -0600

    Fireworks functional

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-"use strict";
+// "use strict";
 
+/** @type {HTMLCanvasElement} */
 const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
@@ -20,6 +21,9 @@ window.addEventListener("resize", () => {
   init();
 })
 
+const GRAVITY = 0.005;
+const FRICTION = 0.99;
+
 // Objects
 class Particle {
   constructor(x, y, radius, color, velocity) {
@@ -28,19 +32,32 @@ class Particle {
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
+    this.alpha = 1;
   }
 
   draw() {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
     ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
+    ctx.restore();
   }
 
   update() {
+    // Multiply x and y velocities by friction
+    this.velocity.x *= FRICTION;
+    this.velocity.y *= FRICTION;
+    // Add gravity to y velocity
+    this.velocity.y += GRAVITY;
+    // Update x and y positions
     this.x += this.velocity.x;
     this.y += this.velocity.y;
+    // Reduce alpha value continuously
+    this.alpha -= 0.005;
+
     this.draw();
   }
 }
@@ -57,10 +74,17 @@ function init() {
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "rgba(0,0,0, .05)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let i = 0; i < particles.length; i++) {
-      particles[i].update();
+    // Remove particle if its alpha val is less than 0.
+    if (particles[i].alpha < 0) {
+        particles.splice(i, 1);
+    } else {
+        particles[i].update();
+    }
   }
 }
 
@@ -80,11 +104,9 @@ document.addEventListener("click", (event) => {
         particles.push(new Particle(
             mouse.x,
             mouse.y,
-            5,
-            "purple",
+            3,
+            `hsl(${Math.random() * 360}, 50%, 50%)`,
             { x: Math.cos(angleIncrement * i) * Math.random(), y: Math.sin(angleIncrement * i) * Math.random() }
         ));
     }
-
-    console.log(particles);
 });
